3913334a4dbf764e8aa5ef9c57aae8797e707bcf
dhrubajit <dhrubajitchakravorty@gmail.com>
2021-12-13 23:53:09 +0000

    deepin.c is modified

--- deepin.c
+++ deepin.c
@@ -110,5 +110,6 @@ int main()
   // Hi this is for git commit..
   // For further changes in this init system we need some help of other contributers so we can make it a public repo after some changes in it..
 	// This is what we call forking it into a repo
+  printf("This is the final end of this script here that will be updated after the further use of it.. \n");
   return 0;
 }
